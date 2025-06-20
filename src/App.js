4d2b19c753c4221d26c2a3e3cb9b893514e1b6c9
Haythem-hms <hemaissihaythem@outlook.com>
2025-06-20 17:32:16 +0200

    Contact set up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import ErrorBoundary from './components/ErrorBoundires'
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<Layout/>}>
         <Route index element={<ErrorBoundary>  <Home/> </ErrorBoundary> }/>
         <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
       </Route>
     </Routes>
     </>
